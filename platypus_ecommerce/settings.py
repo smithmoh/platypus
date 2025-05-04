962d528128fa53c35f6733d8710c472f2dde9ee6
smithmoh <smiqmoses@gmail.com>
2025-05-04 20:02:52 +0100

    settings.py updated

--- platypus_ecommerce/settings.py
+++ platypus_ecommerce/settings.py
@@ -17,7 +17,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rs6gx*h)egp%g6t@j0@82p3x#1lhf=t%hx6b%ia13o6%!3@lqn'
+#SECRET_KEY = 'django-insecure-rs6gx*h)egp%g6t@j0@82p3x#1lhf=t%hx6b%ia13o6%!3@lqn'
+SECRET_KEY = env('SECRET_KEY')
+
 #SECRET_KEY = env('SECRET_KEY', default='django-insecure-rs6gx*h)egp%g6t@j0@82p3x#1lhf=t%hx6b%ia13o6%!3@lqn')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -53,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -92,15 +95,23 @@ WSGI_APPLICATION = 'platypus_ecommerce.wsgi.application'
 #}
 
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'platypus_db',
+#        'USER': 'platypus_user',
+#        'PASSWORD': 'Haust',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'platypus_db',
-        'USER': 'platypus_user',
-        'PASSWORD': 'Haust',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default='postgres://platypus_user:Haust@localhost:5432/platypus_db',
+        conn_max_age=600,
+        ssl_require=not DEBUG
+    )
 }
 
 
